40c6f3d4f70b524bcff0499a1c48cea321aad5a1
Brian Faust <faustbrian@users.noreply.github.com>
2017-12-22 14:33:40 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='ARK Python Client',
+    description='An Ark Client for Python 3',
+    version='1.0.0',
+    author='Brian Faust',
+    author_email='hello@brianfaust.me',
+    url='https://github.com/faustbrian/ARK-Python-Client',
+    packages=['api', 'ark', 'crypto', 'currency'],
+)
