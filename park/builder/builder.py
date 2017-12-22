ccb167d5f5a9d15cd1248316c6ab94b7e280a2ad
galperins4 <galperins4@gmail.com>
2017-12-22 13:14:14 -0600

    Update builder.py
    Kapu support

--- park/builder/builder.py
+++ park/builder/builder.py
@@ -15,7 +15,8 @@ class Builder(ABC):
     def build(self, script, data):
         network = {
             '6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988' : "0x17",
-            '578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23' : "0x1E"
+            '578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23' : "0x1E",
+            '313ea34c8eb705f79e7bc298b788417ff3f7116c9596f5c9875e769ee2f4ede1' : '0x2D'
         }[self.client.nethash]
 
         template = self.env.get_template(script).render({
