85f73941d9299738ef0056fdb53c62790f0281d3
Brian Faust <hello@brianfaust.me>
2017-12-23 07:15:30 +0200

    test a fix

--- setup.py
+++ setup.py
@@ -9,6 +9,25 @@ setup(
     author='Brian Faust',
     author_email='hello@brianfaust.me',
     url='https://github.com/faustbrian/ARK-Python-Client',
-    packages=['park'],
+    packages=[
+        'park'
+        'park.api',
+        'park.builder',
+        # 'park.api.account',
+        # 'park.api.block',
+        # 'park.api.delegate',
+        # 'park.api.loader',
+        # 'park.api.multisignature',
+        # 'park.api.peer',
+        # 'park.api.signature',
+        # 'park.api.transaction',
+        # 'park.api.transport',
+        # 'park.api.vote',
+        # 'park.builder.delegate',
+        # 'park.builder.multisignature',
+        # 'park.builder.signature',
+        # 'park.builder.transaction',
+        # 'park.builder.vote',
+    ],
     install_requires=['Naked', 'Jinja2', 'requests', 'pyyaml', 'MarkupSafe', 'certifi', 'urllib3', 'chardet', 'idna']
 )
