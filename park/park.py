f92a1d656da70edd9a2eda468156d086572fe1fd
galperins4 <galperins4@gmail.com>
2018-06-14 21:32:54 -0500

    Add files via upload

--- park/park.py
+++ park/park.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python
 
-# WORK IN PROGRESS
-from park.one.account import Account
-from park.one.block import Block
-from park.one.delegate import Delegate
-from park.one.loader import Loader
-from park.one.multisignature import MultiSignature
-from park.one.peer import Peer
-from park.one.signature import Signature
-from park.one.transaction import Transaction
-from park.one.transport import Transport
-from park.one.vote import Vote
-
-# WORK IN PROGRESS
-from park.two.account import Account2
-from park.two.block import Block2
-from park.two.delegate import Delegate2
-from park.two.loader import Loader2
-from park.two.multisignature import MultiSignature2
-from park.two.peer import Peer2
-from park.two.signature import Signature2
-from park.two.transaction import Transaction2
-from park.two.transport import Transport2
-from park.two.vote import Vote2
-
-# TO DO - ADD P2P CALLS
+from park.api.account import Account
+from park.api.block import Block
+from park.api.delegate import Delegate
+from park.api.loader import Loader
+from park.api.multisignature import MultiSignature
+from park.api.peer import Peer
+from park.api.signature import Signature
+from park.api.transaction import Transaction
+from park.api.transport import Transport
+from park.api.vote import Vote
 
 from park.builder.delegate import DelegateBuilder
 from park.builder.multisignature import MultiSignatureBuilder
@@ -34,77 +19,45 @@ from park.builder.vote import VoteBuilder
 
 
 class Park:
-    def __init__(self, ip, port, nethash, version, api_version = "v1"):
-        self.connection(ip, port, nethash, version, api_version)
+    def __init__(self, ip, port, nethash, version):
+        self.connection(ip, port, nethash, version)
 
-    def connection(self, ip, port, nethash, version, api_version):
+    def connection(self, ip, port, nethash, version):
         self.ip = ip
         self.port = port
         self.nethash = nethash
         self.version = version
-        self.api_version = api_version
 
     def accounts(self):
-        if self.api_version == "v1":
-            return Account(self)
-        else:
-            return Account2(self)
+        return Account(self)
 
     def blocks(self):
-        if self.api_version == "v1":
-            return Block(self)
-        else:
-            return Block2(self)
+        return Block(self)
 
     def delegates(self):
-        if self.api_version == "v1":
-            return Delegate(self)
-        else:
-            return Delegate2(self)
+        return Delegate(self)
 
     def loaders(self):
-        if self.api_version == "v1":
-            return Loader(self)
-        else:
-            return Loader2(self)
+        return Loader(self)
 
     def multiSignatures(self):
-        if self.api_version == "v1":
-            return MultiSignature(self)
-        else:
-            return MultiSignature2(self)
+        return MultiSignature(self)
 
     def peers(self):
-        if self.api_version == "v1":
-            return Peer(self)
-        else:
-            return Peer2(self)
+        return Peer(self)
 
     def signatures(self):
-        if self.api_version == "v1":
-            return Signature(self)
-        else:
-            return Signature2(self)
+        return Signature(self)
 
     def transactions(self):
-        if self.api_version == "v1":
-            return Transaction(self)
-        else:
-            return Transaction2(self)
+        return Transaction(self)
 
     def transport(self):
-        if self.api_version == "v1":
-            return Transport(self)
-        else:
-            return Transport2(self)
+        return Transport(self)
 
     def votes(self):
-        if self.api_version == "v1":
-            return Vote(self)
-        else:
-            return Vote2(self)
+        return Vote(self)
 
-    # WORK IN PROGRESS
     def delegateBuilder(self):
         return DelegateBuilder(self)
 
